3487d2dc52f80fb798c8fce258046a5e20043a24
shubham <kumarshubham2994@gmail.com>
2021-03-31 22:06:41 +0530

    newww

--- src/main/java/resources/BaseTest.java
+++ src/main/java/resources/BaseTest.java
@@ -16,6 +16,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 
@@ -74,4 +75,7 @@ public class BaseTest {
 		return destinationFile;
 
 	}
+	
+
+
 }
